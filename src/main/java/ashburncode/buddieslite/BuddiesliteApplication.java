a7d4553037766c1de6560384c133074f005ddb08
ashburndev <ashburndev@gmail.com>
2024-07-14 18:43:35 -0400

    initial commit

---
+++ src/main/java/ashburncode/buddieslite/BuddiesliteApplication.java
@@ -0,0 +1,13 @@
+package ashburncode.buddieslite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddiesliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddiesliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ashburncode/buddieslite/BuddiesliteApplicationTests.java
@@ -0,0 +1,13 @@
+package ashburncode.buddieslite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddiesliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
